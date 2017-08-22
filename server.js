1a4dd2932e1ec17bbe16401b8bbe9d8e1cee2c06
Nithuk <31105121+Nithuk@users.noreply.github.com>
2017-08-22 13:38:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content={
+var articleone={
     tilte: 'Article one|Nithya',
     heading:'Article one',
     date:'5th july' ,
