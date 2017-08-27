72832bfbb2f9551aafd3b3b30df2f9856960de84
Nithuk <31105121+Nithuk@users.noreply.github.com>
2017-08-27 16:13:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,19 @@
 console.log('Loaded!');
 
 //Change the text of the main-text div
-var element = document.getElementById('main-text');
+var element = document.getElementById('main-text'
+);
 element.innerHTML = 'New value';
 
 //Move the Image
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight() {
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight,100);
+    var interval = setInterval(moveRight, 50);
     
     
 };
